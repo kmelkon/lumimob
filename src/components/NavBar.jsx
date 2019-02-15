d2ecd7aee8b85156e634a2217f812d6bd664e5fb
Karam Malkon <karammalkon@Karams-MBP.localdomain>
2019-02-15 11:06:01 +0100

    :lipstick:

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,30 @@
 import React from 'react'
 import { NavLink, Link, Switch, Route } from 'react-router-dom'
+import { Button } from '@blueprintjs/core'
 import '../styles/navbar.scss'
 
 const NavBar = props => (
-  <nav className='nav'>
-    <NavLink exact activeClassName='active' className='nav-link' to='/'>
-      Home AKA Lumi
-    </NavLink>
-    <NavLink activeClassName='active' className='nav-link' to='/counter'>
-      Counter
-    </NavLink>
-    <NavLink activeClassName='active' className='nav-link' to='/decorator'>
-      Counter+Decorator
-    </NavLink>
-  </nav>
+  <>
+    <nav className='nav'>
+      <NavLink exact activeClassName='active' className='nav-link' to='/'>
+        Home AKA Lumi
+      </NavLink>
+      <NavLink activeClassName='active' className='nav-link' to='/counter'>
+        Counter
+      </NavLink>
+      <NavLink activeClassName='active' className='nav-link' to='/decorator'>
+        Counter+Decorator
+      </NavLink>
+    </nav>
+    <div className='call-to-actions'>
+      <Button minimal intent='primary'>
+        Import Log
+      </Button>
+      <Button minimal intent='primary'>
+        Users List
+      </Button>
+    </div>
+  </>
 )
 
 export default NavBar
